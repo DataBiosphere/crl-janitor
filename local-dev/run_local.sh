03220c102867d6bf61cf92c3856b6c5b97c132ef
Gregory Jordan <gjordan@verily.com>
2020-09-15 15:22:45 -0400

    Remove most environment interpolation from application.yml and add a PropertyLogger for convenience.

--- local-dev/run_local.sh
+++ local-dev/run_local.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-export DATABASE_USER=dbuser
-export DATABASE_USER_PASSWORD=dbpwd
-export DATABASE_NAME=testdb
-export STACKDRIVER_ENABLED=false
-export STAIRWAY_DATABASE_USER=dbuser_stairway
-export STAIRWAY_DATABASE_USER_PASSWORD=dbpwd_stairway
-export STAIRWAY_DATABASE_NAME=testdb_stairway
-export TRACK_RESOURCE_PUBSUB_ENABLED=false
-export CONFIG_BASED_AUTHZ_ENABLED=false
 ./gradlew bootRun
